791b0518bdfdc6d6dc8ff55e61f1d6e779bc188b
Bernard Duggan <bernard@hippware.com>
2018-08-14 15:46:51 +1000

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GuardianFirebase.MixProject do
   def project do
     [
       app: :guardian_firebase,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       description: description(),
